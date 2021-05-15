1d3fd6c697dfa8d8d0d5bf7be463b2242da661b4
Hamid404 <hamidrmd.59@gmail.com>
2021-05-16 01:25:29 +0200

    changeHeader3

--- src/Header.js
+++ src/Header.js
@@ -2,14 +2,10 @@ import React from "react";
 import "./style.css";
 
 const Header =()=>{
-    return <div className={"header"}>
-    <h1 >
-         Hamid Post Page
-    </h1>
-    <p>Can You be success? Why not?</p>
-    <br></br>
-    <p>Than follow my 10 suggestions.</p>
-  </div>
+    return <p className={"header"}>
+    
+         Success
+ </p>
 }
 
 export default Header
